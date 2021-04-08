14b6a95650a866431141a04d6cacbd78b0a2239e
Ashish Subedi <ashishsubedi10@gmail.com>
2021-04-08 18:01:28 +0545

    Detail View

--- gym_management/urls.py
+++ gym_management/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 from .views import (
     StatsView,
+    StatsDetailView,
 
     mark_all_expired_inactive,
 )
@@ -12,5 +13,7 @@ app_name = 'gym_management'
 
 urlpatterns = [
     path('stats/', staff_member_required(StatsView.as_view()), name='stats_view'),
+    path('stats/<str:member_type>', staff_member_required(StatsDetailView.as_view()), name='stats_detail_view'),
+
     path('make_inactive/',mark_all_expired_inactive,name='mark_expired_inactive')
 ]
--- gym_management/views.py
+++ gym_management/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.core.exceptions import PermissionDenied
-from django.shortcuts import get_object_or_404, reverse, redirect,render
+from django.shortcuts import get_object_or_404, reverse, redirect, render
 
 from django.http import HttpResponse, HttpResponseNotFound
 from django.views import View
 
-from django.urls import path, re_path,reverse_lazy
+from django.urls import path, re_path, reverse_lazy
 
-from .models import User,Invoice, MembershipType
+from .models import User, Invoice, MembershipType
 from django.db.models import Q
 from django.contrib.admin.views.decorators import staff_member_required
 from django.utils import timezone
@@ -20,29 +20,60 @@ def seven_days_hence():
 
 
 class StatsView(View):
-    def get(self,request):
-        total_members = User.objects.filter(is_staff=False,is_superuser=False)
-        active_members = total_members.filter(membership_status=0,expires_at__gte=timezone.now())
-        expired_members = total_members.filter(membership_status=0,expires_at__lte=timezone.now())
-        expiring_members = total_members.filter(Q(membership_status=0) & Q(expires_at__lte=seven_days_hence()) & Q(expires_at__gte=timezone.now()))
-        print(active_members,expired_members,expiring_members)
+    def get(self, request):
+        total_members = User.objects.filter(is_staff=False, is_superuser=False)
+        active_members = total_members.filter(
+            membership_status=0, expires_at__gte=timezone.now())
+        expired_members = total_members.filter(
+            membership_status=0, expires_at__lte=timezone.now())
+        expiring_members = total_members.filter(Q(membership_status=0) & Q(
+            expires_at__lte=seven_days_hence()) & Q(expires_at__gte=timezone.now()))
         context = {
             'total_members': total_members,
             'active_members': active_members,
             'expired_members': expired_members,
             'expiring_members': expiring_members,
         }
-        return render(request,'stats/index.html', context=context)
+        return render(request, 'stats/index.html', context=context)
+
 
 @staff_member_required
 def mark_all_expired_inactive(request):
-    expired_members = User.objects.filter(is_staff=False,is_superuser=False).filter(Q(expires_at__lte=timezone.now()))
+    expired_members = User.objects.filter(
+        is_staff=False, is_superuser=False).filter(Q(expires_at__lte=timezone.now()))
     expired_members.update(membership_status=1)
-    # for q in expired_members:
-    #     q.save()
-    print('updated')
-    messages.add_message(request, messages.SUCCESS, 'All expired users are deactivated.')
+
+    messages.add_message(request, messages.SUCCESS,
+                        'All expired users are deactivated.')
 
     return redirect(reverse_lazy('gym_management:stats_view'))
-    
-    
+
+
+class StatsDetailView(View):
+ 
+    MEMBERS_MAP = {
+        'total_members':  User.objects.filter(is_staff=False, is_superuser=False),
+        'active_members': User.objects.filter(is_staff=False, is_superuser=False).filter(
+            membership_status=0, expires_at__gte=timezone.now()),
+        'expired_members': User.objects.filter(is_staff=False, is_superuser=False).filter(
+            membership_status=0, expires_at__lte=timezone.now()),
+        'expiring_members': User.objects.filter(is_staff=False, is_superuser=False).filter(Q(membership_status=0) & Q(
+            expires_at__lte=seven_days_hence()) & Q(expires_at__gte=timezone.now())),
+    }
+
+    def get(self, request, member_type):
+        try:
+            
+            if not member_type in self.MEMBERS_MAP.keys():
+                raise Exception("Invalid Request")
+
+            result = self.MEMBERS_MAP[member_type]
+            print(member_type,result)
+            ##Give output as datatable
+
+            return redirect(reverse_lazy('gym_management:stats_view'),)
+        except Exception as e:
+            messages.add_message(request, messages.WARNING, e)
+            return redirect(reverse_lazy('gym_management:stats_view'))
+
+            print(e)
